b493f21eaa2c96a41d75c278caa81487dd51021e
Tiago França <tfranca2@gmail.com>
2014-09-18 14:08:00 -0300

    commit inicial

---
+++ PCP/src/com/exemplo/testes/Teste.java
@@ -0,0 +1,32 @@
+package com.exemplo.testes;
+
+
+public class Teste {
+
+	public static void main(String[] args) {
+//		RepositorioCliente repositorioCliente = new RepositorioCliente();
+		
+//		Cliente cliente = new Cliente();
+//		cliente.setNome("Beltrano da Silva");
+//		cliente.setIdade(20);
+//		repositorioCliente.salvar(cliente);
+	
+//		List<Cliente> clientes = repositorioCliente.listarTodos();
+//		for (Cliente cliente : clientes) {
+//			System.out.println(cliente.getNome());
+//		}
+		
+//		Cliente cliente = repositorioCliente.ObterPorId(2);
+//		System.out.println(cliente.getNome());
+
+//		Cliente cliente = repositorioCliente.ObterPorId(2);
+//		cliente.setNome("Beltrano de Souza");
+//		repositorioCliente.salvar(cliente);		
+//		System.out.println(cliente.getNome());
+		
+//		Cliente cliente = repositorioCliente.ObterPorId(2);
+//		repositorioCliente.remover(cliente);
+		
+	}
+
+}
