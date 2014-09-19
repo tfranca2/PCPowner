30fde3b7b99a37d91b1cb6286f530826379316ac
jocelio.lima <jocelio.lima@cti-315069.SME.FORTALEZA.CE.GOV.BR>
2014-09-19 14:25:39 -0300

    Configuração JPA

--- PCPWeb/src/com/phpweb/view/ControleCliente.java
+++ PCPWeb/src/com/phpweb/view/ControleCliente.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.model.DataModel;
 
 import com.exemplo.entidade.Cliente;
 import com.exemplo.repositorio.RepositorioCliente;
